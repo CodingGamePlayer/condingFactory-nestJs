e793f652bab1fced1bf29475e2a4fa01e43d8f51
김종식 <kim.jongsik@lumir-inc.com>
2023-11-06 10:11:19 +0900

    3_2 controller
    - 프로젝트 시작

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,17 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHome() {
-    return 'Home Page';
-  }
-
-  @Get('post')
-  getPost() {
-    return 'Post Page';
-  }
-
-  @Get('user')
-  getUser() {
-    return 'User Page';
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
